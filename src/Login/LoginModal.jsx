08fd65d6ab8f8629c545777f4ab0192ef5418539
Jeffrey Doyle <jeffreydoylec@gmail.com>
2019-10-21 19:05:02 -0700

    Added very rough initial implementation

--- src/Login/LoginModal.jsx
+++ src/Login/LoginModal.jsx
@@ -3,12 +3,10 @@ import React from 'react';
 const SignInModal = () => (
   <div className="SignInModal">
       <div className="SignInModalInner">
-          <div className="SignInModalTitle">Connect To Your Bank</div>
+          <div className="SignInModalTitle">Login with HSBC</div>
           <div className="SignInModalList">
-              <div className="SignInModalListOption">HSBC</div>
-              <div className="SignInModalListOption">Other Bank 1</div>
-              <div className="SignInModalListOption">Other Bank 2</div>
-              <div className="SignInModalListOption">Other Bank 3</div>
+              <input type="email" className="SignInModalListInput" placeholder="email" />
+              <input type="password" className="SignInModalListInput" placeholder="password" />
           </div>
       </div>
   </div>
